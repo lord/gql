381dacc9bea71dbb5440dc9e4f3c5ebbf6f2a95f
Robert Lord <robert@lord.io>
2016-10-26 10:26:48 -0500

    Add ignored tokens

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,35 @@
 #[macro_use]
 extern crate nom;
 
-amed!(white_space<&str, &str>, alt!(tag_s!(" ") | tag_s!("\n")));
-named!(letter, tag!("meow"));
-named!(parens< &[u8], Vec<&[u8]> >, delimited!(char!('('), many0!(letter), char!(')')));
+// Ignored tokens
+fn line_terminator_char(x: char) -> bool {
+  x == '\u{000D}' || x == '\u{000A}'
+}
+named!(unicode_bom<&str, &str>, tag_s!("\u{feff}"));
+named!(white_space<&str, &str>, alt!(tag_s!(" ") | tag_s!("\t")));
+named!(line_terminator<&str, &str>, alt!(
+  tag_s!("\u{000D}\u{000A}") |
+  tag_s!("\u{000D}") |
+  tag_s!("\u{000A}")));
+named!(comment<&str, &str>, preceded!(
+  tag_s!("#"),
+  take_till_s!(line_terminator_char)));
+named!(comma<&str, &str>, tag_s!(","));
+named!(ignored<&str, &str>, alt_complete!(
+  unicode_bom |
+  white_space |
+  line_terminator |
+  comment |
+  comma));
+// named!(letter, tag!("meow"));
+// named!(parens< &[u8], Vec<&[u8]> >, delimited!(char!('('), many0!(letter), char!(')')));
 
 #[cfg(test)]
 mod tests {
-    use super::white_space;
+    use super::ignored;
     #[test]
     fn it_works() {
-      println!("meow");
-      println!("{:?}", white_space(&"\n".to_string()));
+      println!("{:?}", ignored(&" ".to_string()));
       assert!(false)
     }
 }
